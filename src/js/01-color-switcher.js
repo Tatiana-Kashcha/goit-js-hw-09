9a04b047235f7c7d1ca187e8485aa1f0e1a8628a
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-12 23:39:49 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId;
+
+stopBtn.setAttribute('disabled', true);
+
+const onStartBtnClick = () => {
+  timerId = setInterval(() => {
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled', true);
+    const colorBody = getRandomHexColor();
+    bodyEl.style.backgroundColor = colorBody;
+  }, 1000);
+};
+startBtn.addEventListener('click', onStartBtnClick);
+
+const onStopBtnClick = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled', true);
+  stopBtn.setAttribute('disabled', true);
+};
+stopBtn.addEventListener('click', onStopBtnClick);
