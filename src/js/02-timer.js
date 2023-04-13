60d323774e6c95273ded345a6e86efeda97a4abf
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-13 15:36:31 +0300

    додає оформлення таймера

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,3 +8,14 @@ const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr(inputDate, options);
