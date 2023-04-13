307e5391da566e08b876e5848a3101a95577067f
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-13 12:55:31 +0300

    додає бібліотеки

---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputDate = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
