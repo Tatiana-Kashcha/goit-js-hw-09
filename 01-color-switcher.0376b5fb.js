a0784edc72198a70b3af69e89af30a2190ea527a
Tatiana-Kashcha <116923287+Tatiana-Kashcha@users.noreply.github.com>
2023-04-16 12:02:44 +0000

    Deploying to gh-pages from @ Tatiana-Kashcha/goit-js-hw-09@1c5c6a549ed1480235eb4835c56e89c27df49f29 🚀

--- 01-color-switcher.0376b5fb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let d;r.setAttribute("disabled",!0);e.addEventListener("click",(()=>{e.setAttribute("disabled",!0),r.removeAttribute("disabled",!0),d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)}));r.addEventListener("click",(()=>{clearInterval(d),e.removeAttribute("disabled",!0),r.setAttribute("disabled",!0)}));
-//# sourceMappingURL=01-color-switcher.0376b5fb.js.map
